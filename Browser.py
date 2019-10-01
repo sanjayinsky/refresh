e840ca1eb5a24a587ed1a80931a5b8c8fbc5d91a
sanjayinsky <56031298+sanjayinsky@users.noreply.github.com>
2019-10-01 20:14:07 +0530

    URL Refresh

---
+++ Browser.py
@@ -0,0 +1,8 @@
+import webbrowser #allows opening webpages
+import time #uses sleep to delay loop, to make time for user to interact
+import random
+
+while True:
+	visit = 'https://google.com'
+	webbrowser.open(visit)
+	seconds = random.randrange(5, 20)
